8a4036426ffddf3f7d052e3e201d514e1bba2be3
Kaarina <k1j20@tulleau.com>
2023-06-02 22:44:17 -0700

    Added all bars & month labels

--- main.js
+++ main.js
@@ -35,14 +35,11 @@ let tempCol2 = 'actual_min_temp';
 let precCol1 = 'actual_precipitation';
 let precCol2 = 'average_precipitation';
 let year = "2014";
-//let minFreq;
-//let maxFreq;
 
 // Global function called when selected location is changed
 function onCityChanged() {
     let catSelect = d3.select('#citySelect').node();
 
-    // Get current value of select element
     city = catSelect.options[catSelect.selectedIndex].value;
 
     updateChart(city, tempCol1, tempCol2, precCol1, precCol2, year);
@@ -51,7 +48,6 @@ function onCityChanged() {
 function onCol1Changed() {
     let catSelect = d3.select('#col1Select').node();
 
-    // Get current value of select element
     tempCol1 = catSelect.options[catSelect.selectedIndex].value;
 
     updateChart(city, tempCol1, tempCol2, precCol1, precCol2, year);
@@ -60,7 +56,6 @@ function onCol1Changed() {
 function onCol2Changed() {
     let catSelect = d3.select('#col2Select').node();
 
-    // Get current value of select element
     tempCol2 = catSelect.options[catSelect.selectedIndex].value;
 
     updateChart(city, tempCol1, tempCol2, precCol1, precCol2, year);
@@ -69,7 +64,6 @@ function onCol2Changed() {
 function onPrec1Changed() {
     let catSelect = d3.select('#prec1Select').node();
 
-    // Get current value of select element
     precCol1 = catSelect.options[catSelect.selectedIndex].value;
 
     updateChart(city, tempCol1, tempCol2, precCol1, precCol2, year);
@@ -78,7 +72,6 @@ function onPrec1Changed() {
 function onPrec2Changed() {
     let catSelect = d3.select('#prec2Select').node();
 
-    // Get current value of select element
     precCol2 = catSelect.options[catSelect.selectedIndex].value;
 
     updateChart(city, tempCol1, tempCol2, precCol1, precCol2, year);
@@ -87,9 +80,7 @@ function onPrec2Changed() {
 function onYearChanged() {
     let catSelect = d3.select('#yearSelect').node();
 
-    // Get current value of select element
     year = catSelect.options[catSelect.selectedIndex].value;
-    console.log(year);
 
     updateChart(city, tempCol1, tempCol2, precCol1, precCol2, year);
 }
@@ -102,16 +93,8 @@ let svgHeight = +svg.attr('height');
 
 let padding = {t: 60, r: 40, b: 30, l: 40};
 
-// Compute chart dimensions
-/*let chartWidth = svgWidth - padding.l - padding.r;
-let chartHeight = svgHeight - padding.t - padding.b;*/
-
 let chartWidth = svgWidth - padding.l - padding.r;
 let chartHeight = svgHeight - padding.t - padding.b;
-
-// Compute the spacing for bar bands based on all 26 letters
-/*let barBand = chartHeight / 26;
-let barHeight = barBand * 0.7;*/
 
 let barBand = chartWidth / 26;
 let barWidth = barBand * 0.7;
@@ -120,18 +103,9 @@ let barWidth = barBand * 0.7;
 let chartG = svg.append('g')
     .attr('transform', 'translate('+[padding.l, padding.t]+')');
 
-// add a chart title
-svg.append('g')
-   .attr('class', 'title')
-   .attr('transform', 'translate('+[chartWidth / 3 + 20, 30]+')')
-   .append('text')
-   .text('Weather')
-   .style('font-size', '14px');
-
 d3.csv('./DataProcessing/final.csv').then(function(dataset) {
-    // Create global variables here and intialize the chart
 
-    // initialize letters
+    // initialize weather
     weather = dataset;
 
     // find min and max values for actual_mean_temp (CHANGE THIS LATER TO BE FLEXIBLE ON FILTER)
@@ -149,13 +123,11 @@ d3.csv('./DataProcessing/final.csv').then(function(dataset) {
     freqScale = d3.scaleLinear().domain(weather.map(d => d.date)).range([0, chartWidth]);
 
     // set up y scale for bars
-    //yScale = d3.scaleLinear().domain([0, weather.length]).range([0, chartHeight]);
     yScale = d3.scaleLinear()
         .domain([0, maxTemp])
         .range([0, chartHeight]);
 
     // call updateChart function
-    //updateChart('all-letters', minFreq * 100, maxFreq * 100);
     updateChart(city, tempCol1, tempCol2, precCol1, precCol2, year);
 });
 
@@ -166,65 +138,60 @@ function updateChart(selected_city, tempColumn1, tempColumn2, precColumn1, precC
 
     console.log(filteredData);
 
-    // create bars for each data point (for now)
-    let bars = chartG.selectAll('rect')
-                     .data(filteredData, function(d){
-                        return d.actual_mean_temp;
-                    })
-    
-    /*
-    bars.enter()
-        .append('rect')
-        .attr('class', 'bar')
-        .attr('x', 0)
-        .merge(bars)
-        .attr('y', function(d, i) {
-            return yScale(i);
-        })
-        .attr('width', d => freqScale(d.actual_mean_temp))
-        .attr('height', barHeight)
-        .attr('fill', 'black');
-    */
+    let rectWidth = (680 - (filteredData.length * 50)) / filteredData.length;
 
-    bars.enter()
-        .append('rect')
-        .attr('class', 'bar')
-        .merge(bars)
-        .attr('x', 0)
-        .attr('y', d => yScale(d.actual_mean_temp))
-        .attr('width', barWidth)
-        .attr('height', d => chartHeight - yScale(d.actual_mean_temp))
-        .attr('fill', 'black');
+    // remove all rectangles in order to regenerate them
+    let rectangles = svg.selectAll('rect');
+    rectangles.remove();
 
-    bars.exit().remove(); // remove unnecessary bars
+    // remove all month labels
+    let text = svg.selectAll('text');
+    text.remove();
 
-    // create label for each letter
-    let labels = chartG.selectAll('text')
-                       .data(filteredData, function(d) {
-                        return d.month_name;
-                    })
-    
-    
-    /*
-    labels.enter()
-          .append('text')
-          .attr("class", "label")
-          .merge(labels)
-          .text(d => d.letter)
-          .attr("x", -barBand)
-          .attr("y", function(d, i) { return yScale(i) + barHeight / 2 + 5; })
-    */
-    labels.enter()
+    // add a chart title
+    svg.append('g')
+        .attr('class', 'title')
+        .attr('transform', 'translate('+[chartWidth / 3 + 20, 30]+')')
         .append('text')
-        .attr('class', 'label')
-        .merge(labels)
-        .text(d => d.letter)
-        .attr('x', function(d, i) {
-        return barWidth / 2;
-        })
-        .attr('y', d => yScale(d.actual_mean_temp))
+        .text('Weather')
+        .style('font-size', '16px');
+
+    for (i = 0; i < filteredData.length; i++) {
+        // append one rectangle for tempColumn1
+        chartG.append('rect')
+            .attr('x', 50 + (rectWidth + 50)*i)
+            .attr('y', 200 - (filteredData[i][tempColumn1] * 2))
+            .attr('width', 50)
+            .attr('height', filteredData[i][tempColumn1] * 2)
+            .attr('fill', 'blue');
+        // append another rectangle for tempColumn2
+        chartG.append('rect')
+            .attr('x', 100 + (rectWidth + 50)*i)
+            .attr('y', 200 - (filteredData[i][tempColumn2] * 2))
+            .attr('width', 50)
+            .attr('height', filteredData[i][tempColumn2] * 2)
+            .attr('fill', 'red');
+        // append a rectangle for precColumn1
+        chartG.append('rect')
+            .attr('x', 50 + (rectWidth + 50)*i)
+            .attr('y', 300)
+            .attr('width', 50)
+            .attr('height', filteredData[i][precColumn1] * 50)
+            .attr('fill', 'green');
+        // append another rectangle for tempColumn2
+        chartG.append('rect')
+            .attr('x', 100 + (rectWidth + 50)*i)
+            .attr('y', 300)
+            .attr('width', 50)
+            .attr('height', filteredData[i][precColumn2] * 50)
+            .attr('fill', 'black');
+        // add text
+        chartG.append('text')
+        .text(filteredData[i].month_name)
+        .attr('x', 75 + (rectWidth + 50) * i + 25)
+        .attr('y', 250)
         .attr('text-anchor', 'middle')
-        .attr('dominant-baseline', 'middle');
-    
-    labels.exit().remove();
+        .attr('font-size', '16px')
+        .attr('fill', 'black');
+    }
 };
